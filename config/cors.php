2a016f00a6c2551eb13f2469dcf9ff95524bc27e
Floriane HESSOUH <floriane.hessouh@gmail.com>
2026-06-21 01:44:44 +0100

    fix(cors): allow Vercel frontend domain

--- config/cors.php
+++ config/cors.php
@@ -13,10 +13,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://supmin-front.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
